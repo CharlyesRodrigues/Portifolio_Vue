56fa0f8c1bfdf945b056bad24c81eb8c99bc1607
CharlyesRodrigues <charlyessouza@gmail.com>
2022-12-10 14:02:11 -0300

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
   <div class=" absolute w-[11.875rem] h-10 left-[39.0625rem] top-4  bg-white-33%  rounded-lg "></div>
   
   <div class=" absolute w-[9.375rem]  h-[1.73875] left-[40.3125rem] top-[1.375rem]">
-     <img src="./../assets/Logo/image.png">
+
   </div>
  
  
